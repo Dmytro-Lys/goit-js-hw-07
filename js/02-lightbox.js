bd681d6655a8ad5f86be5e4202ea1cebadcf214f
Dmytro-Lys <d.lysenko122@gmail.com>
2023-05-14 10:45:51 +0300

    02-lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const createGalleryItem = (largeImg, smallImg, imgDescription) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${largeImg}">
+    <img
+      class="gallery__image"
+      src="${smallImg}"
+      alt="${imgDescription}"
+    />
+  </a>
+</li>`};
+
+const render = () => {
+    const images = galleryItems.map(({ original, preview, description }) => createGalleryItem(original, preview, description));
+    gallery.insertAdjacentHTML('beforeend', images.join(''));
+}
+render();
+
+const gallerySLb = new SimpleLightbox('.gallery a');
+gallerySLb.options.captionsData = "alt";
+gallerySLb.options.captionDelay = "250";
